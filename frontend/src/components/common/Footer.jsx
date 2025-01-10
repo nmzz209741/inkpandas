7c2c5d505cb19846af6846bcc0f9cb2903ffaa30
Shreeti Shrestha <shreetis13@gmail.com>
2025-01-10 21:40:01 +0545

    feat(footer): footer complete

--- frontend/src/components/common/Footer.jsx
+++ frontend/src/components/common/Footer.jsx
@@ -1,5 +1,44 @@
+import { Box, IconButton } from "@mui/material";
+import { SOCIAL_MEDIA } from "../../configs/constants";
+import { useLocation } from "react-router-dom";
+
+const styles = {
+  footerContainer: (isAuthPage) => ({
+    bgcolor: isAuthPage ? "secondary.main" : "primary.main",
+    color: isAuthPage ? "primary.main" : "secondary.main",
+    height: 60,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    p: 1,
+  }),
+};
+
 const Footer = () => {
-  return <div>Footer</div>;
+  const location = useLocation();
+  const isAuthPage =
+    location.pathname.includes("/login") ||
+    location.pathname.includes("/register");
+  return (
+    <Box component="footer" sx={styles.footerContainer(isAuthPage)}>
+      {SOCIAL_MEDIA.map((social) => (
+        <IconButton
+          key={social.id}
+          href={social.url}
+          target="_blank"
+          rel="noreferrer"
+          sx={{ color: "inherit", mx: 0.5 }}
+          aria-label={social.name}
+        >
+          {social.icon}
+        </IconButton>
+      ))}
+      <Box sx={{ flexGrow: 1 }}></Box>
+      <p>
+        &copy; {new Date().getFullYear()} @nmzz209741. Crafted with &#x2665;
+      </p>
+    </Box>
+  );
 };
 
 export default Footer;
---
+++ frontend/src/configs/constants.jsx
@@ -0,0 +1,16 @@
+import { Instagram, GitHub } from "@mui/icons-material";
+
+export const SOCIAL_MEDIA = [
+  {
+    id: 1,
+    name: "GitHub",
+    url: "https://www.instagram.com/shreeti_nmzz/",
+    icon: <GitHub />,
+  },
+  {
+    id: 2,
+    name: "Instagram",
+    url: "https://github.com/nmzz209741",
+    icon: <Instagram />,
+  },
+];
