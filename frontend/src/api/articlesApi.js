1e0e1847bb02245821b714c058bbe0c9c88b4d7b
Shreeti Shrestha <shreetis13@gmail.com>
2025-01-10 15:04:42 +0545

    feat(api): add basic apis

---
+++ frontend/src/api/articlesApi.js
@@ -0,0 +1,61 @@
+import api from "../services/api";
+
+const getAllArticles = async (page, limit = 50) => {
+  try {
+    const response = await api.get("/articles", { params: { page, limit } });
+    return response.data;
+  } catch (error) {
+    throw new Error(
+      error.response?.data?.message || "Failed to fetch articles"
+    );
+  }
+};
+
+const getArticleById = async (id) => {
+  try {
+    const response = await api.get(`/articles/${id}`);
+    return response.data;
+  } catch (error) {
+    throw new Error(error.response?.data?.message || "Failed to fetch article");
+  }
+};
+
+const createArticle = async (data) => {
+  try {
+    const response = await api.post("/articles", data);
+    return response.data;
+  } catch (error) {
+    throw new Error(
+      error.response?.data?.message || "Failed to create article"
+    );
+  }
+};
+
+const updateArticle = async (id, data) => {
+  try {
+    const response = await api.patch(`/articles/${id}`, data);
+    return response.data;
+  } catch (error) {
+    throw new Error(
+      error.response?.data?.message || "Failed to update article"
+    );
+  }
+};
+
+const deleteArticle = async (id) => {
+  try {
+    await api.delete(`/articles/${id}`);
+  } catch (error) {
+    throw new Error(
+      error.response?.data?.message || "Failed to delete article"
+    );
+  }
+};
+
+export {
+  getAllArticles,
+  getArticleById,
+  createArticle,
+  updateArticle,
+  deleteArticle,
+};
---
+++ frontend/src/api/authApi.js
@@ -0,0 +1,23 @@
+import api from "../services/api";
+
+const register = async (credentials) => {
+  try {
+    const response = await api.post("/auth/register", credentials);
+    return response.data;
+  } catch (error) {
+    throw new Error(
+      error.response?.data?.message || "User Registration failed"
+    );
+  }
+};
+
+const login = async (credentials) => {
+  try {
+    const response = await api.post("/auth/login", credentials);
+    return response.data;
+  } catch (error) {
+    throw new Error(error.response?.data?.message || "User Login failed");
+  }
+};
+
+export { register, login };
