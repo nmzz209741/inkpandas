10c33a477933ef24469af8ca11417b4aadc52a11
Shreeti Shrestha <shreetis13@gmail.com>
2025-01-14 13:59:31 +0545

    fix(error): Fix error layout and error page

--- frontend/src/components/layouts/ErrorLayout.jsx
+++ frontend/src/components/layouts/ErrorLayout.jsx
@@ -1,7 +1,30 @@
+import { Box, Button } from "@mui/material";
+import notFound from "../../assets/images/404.png";
+const styles = {
+  errorContainer: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column",
+    width: "100%",
+    height: "100vh",
+    backgroundColor: "secondary.main",
+  },
+};
+
 const ErrorLayout = () => {
   return (
-    <div>ErrorLayout</div>
-  )
-}
+    <Box sx={styles.errorContainer}>
+      <img src={notFound} alt="404" style={{ width: "50%" }} />
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => window.location.replace("/")}
+      >
+        Take Me Home
+      </Button>
+    </Box>
+  );
+};
 
-export default ErrorLayout
+export default ErrorLayout;
